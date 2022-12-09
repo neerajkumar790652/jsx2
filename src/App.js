790824d3d68b32717709642486f6b3ff3e4da509
neerajkumar790652 <92433406+neerajkumar790652@users.noreply.github.com>
2022-12-09 13:56:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+     <div className="container">
+       <h1>The Evolution of Airbnb's Frontend</h1>
+       <h3>@spikebrehm</h3>
+       <img src="https://res.cloudinary.com/dtzgowi4u/image/upload/v1670573211/unnamed_emcn9a.png" alt="image"/>
+     </div>
+    </div>
+  );
+}
